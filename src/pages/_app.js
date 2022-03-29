5c59dc74909144cc1dee5108909af715c97d5170
Eriberto SG <santosbxs1999@gmail.com>
2022-03-29 12:46:16 -0600

    Se creo el componente _app

---
+++ src/pages/_app.js
@@ -0,0 +1,11 @@
+import 'normalize.css'
+
+function myApp({ Component, pageProps }) {
+    return (
+        <>
+            <Component {...pageProps} />
+        </>
+    )
+}
+
+export default myApp
