df6f9563bea2d385fb2b4b550cfbe75ce5a85171
Eriberto SG <santosbxs1999@gmail.com>
2022-04-01 11:55:37 -0600

    se agrego el router en el index, ademas se listaron las tareas que hay.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,29 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+
 import { useTask } from '../components/customHooks/useTask'
+import Card from '../components/molecules/Card'
 
 function Home() {
+    const { push } = useRouter()
     const { tasks } = useTask()
 
     return (
         <div>
             <header>
                 <span>{tasks.length} task</span>
+                <Link href="/new">
+                    <a>Create new Task</a>
+                </Link>
             </header>
+            <hr />
             <main>
-                <h1>Main</h1>
+                {tasks.length === 0 ? <h1>No hay tareas</h1> : (
+                    tasks.map((task, index) =>
+                        <Card key={task.id} index={index + 1} title={task.title} description={task.description} onClick={() => push(`/edit/${task.id}`)} />
+                    )
+                )
+                }
             </main>
         </div>
     )
