42ac57f5d9fb1d2b59b36c95dd593ac337d9ccdb
Eriberto SG <santosbxs1999@gmail.com>
2022-04-02 20:57:45 -0600

    Se agregaron iconos

--- src/components/orgnisms/Nav.jsx
+++ src/components/orgnisms/Nav.jsx
@@ -13,8 +13,13 @@ const Nav = () => {
                 </div>
                 <div className="header__nav-rigth">
                     {router.pathname === '/new' || router.pathname === '/edit/[id]'
-                        ? ''
-                        : <button onClick={() => router.push('/new')} className="button">Create New Task</button>
+                        ?
+                        ''
+                        :
+                        <button onClick={() => router.push('/new')} className="button flex">
+                            <span>Create New Task</span>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="plus"><path d="M19 11h-6V5h-2v6H5v2h6v6h2v-6h6z"></path></svg>
+                        </button>
                     }
                 </div>
             </div>
