221c3e023b8f28510a5262010e4e2829e3c4f2c1
Eriberto SG <santosbxs1999@gmail.com>
2022-04-04 12:34:17 -0500

    Se agregaron los archivos principales en la carpeta pages

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+        </View>
+    );
+}
