9a2640dde159edbe41bd8c90c751afd4d13b50f9
EribertoSG <eribertosg.dev@gmail.com>
2022-08-06 14:36:01 -0500

    capturing data form

--- src/components/organisms/Form.jsx
+++ src/components/organisms/Form.jsx
@@ -1,10 +1,21 @@
+import { useState } from "react"
+
 const Form = () => {
+    const initialState = {
+        title: '',
+        description: ''
+    }
+    const [data, setData] = useState(initialState)
+    const handleChange = ({ target: { name, value } }) => {
+        setData({ ...data, [name]: value })
+    }
+
     return (
         <div className="form__container">
             <h1>Form</h1>
             <form className="form">
-                <input className="input" type="text" name="title" id="title" placeholder="Title" />
-                <textarea className="textarea" name="description" id="description" cols="30" rows="10" placeholder="Description"></textarea>
+                <input className="input" type="text" name="title" id="title" placeholder="Title" onChange={handleChange} />
+                <textarea className="textarea" name="description" id="description" cols="30" rows="10" placeholder="Description" onChange={handleChange}></textarea>
                 <button className="button full" type='submit'>add</button>
             </form>
         </div>
